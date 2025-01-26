53a3a42184a26fd6204dd65258af155dda69c5bc
janvi <laxmijaswal189@gmail.com>
2025-01-26 02:10:30 -0500

    craete a riute if update user

--- backend/routers/userRoutes.js
+++ backend/routers/userRoutes.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const userControllers = require("../controllers/user-controllers");
-const protect = require('../middleware/authMiddleware')
+const protect = require("../middleware/authMiddleware");
+
 router.route("/register").post(userControllers.registerUser);
 router.route("/login").post(userControllers.loginUser);
 router.route("/logout").get(userControllers.logout);
 router.route("/getUser").get(protect, userControllers.getUser);
-router.route("/loggedin").get(protect, userControllers.loginStatus);
+router.route("/loggedin").get(userControllers.loginStatus);
+router.route("/updateuser").patch(protect, userControllers.updateUser);
 
 module.exports = router;
