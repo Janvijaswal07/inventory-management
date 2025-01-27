26d86f82d424285a99856e6b20233acad1bde9c7
janvi <laxmijaswal189@gmail.com>
2025-01-27 03:53:02 -0500

    create middleware

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -11,14 +11,14 @@ const protect = asyncHandler(async (req, res, next) => {
     }
 
     const verified = jwt.verify(token, process.env.JWT_SECRET);
-    console.log(verified);
+   
     const user = await User.findById(verified.id).select("-password");
 
     if (!user) {
       res.status(401);
       throw new Error("User not found");
     }
-    console.log(user);
+    
     req.user = user;
     next();
   } catch (error) {
